2b30f08f3f10f7340ebd4c78cb258a0289845c11
Ronald <ronald@rangel.pro>
2025-11-07 16:03:49 -0500

    gaa

--- src/app/dashboard/[chatbot]/chats/page.js
+++ src/app/dashboard/[chatbot]/chats/page.js
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card"
 import Link from "next/link"
 import IframeWithPreloader from "@/components/iframe-with-preloader"
 
@@ -9,25 +10,27 @@ export default function ChatsPage({ params }) {
       <div className="@container/main flex flex-1 flex-col gap-2">
 
         <div className="flex flex-col">
-
-
-          <div className="relative w-full h-[calc(100vh)]">
-            <IframeWithPreloader
-              src="https://crm.eliteseller.app/"
-              title="Chats embebidos"
-              className="w-full h-full"
-            />
-          </div>
-
           <div className="p-4">
-            <Button asChild>
-              <Link href="/api/crm/login?redirect=1" target="_blank" rel="noopener noreferrer">
-                Accede a tus chats
-              </Link>
-            </Button>
-
+            <Card className="max-w-lg">
+              <CardHeader>
+                <CardTitle>Accede a tus chats</CardTitle>
+                <CardDescription>
+                  Ahí puedes gestionar las conversaciones con tus clientes.
+                </CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <Button className="w-full" asChild>
+                  <Link
+                    href="/api/crm/login?redirect=1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Abrir chats
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
           </div>
-
         </div>
       </div>
     </div>
